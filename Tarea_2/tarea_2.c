08fcf81dc88286753f1a479ffd2a17f3b1f65f0b
Roberto Rocuant <r.rocuant4@hotmail.com>
2016-12-05 06:24:24 -0300

    Update tarea_2.c

--- Tarea_2/tarea_2.c
+++ Tarea_2/tarea_2.c
@@ -48,7 +48,7 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	//Este es el promedio de la lista completa
+	//Éste es el promedio de la lista completa
 	float promedio=0.0;
 
 	//Generamos números aleatorios entre 0 y 1, el tamanño de la muestra es 100
@@ -64,13 +64,13 @@ int main(int argc, char** argv) {
 	int reajuste=reajuste_asignacion(100,size);
 	float sumas_reajustes=0.0;
 
-	//Calculamos el tamanho total por subdivision de la lista, en el caso de tamanho 100 la subdivision es de 25
+	//Calculamos el tamanho total por subdivision de la lista, en el caso de tamanño 100 la subdivisión es de 25
 	int div_tamanho=100/size;
 	
 	//Nos ubicamos en el nodo maestro rank=0
         if(rank == root) {
 
-		//Aca le damos el tamaño de la lista que usaremos para guardar los datos en la raiz, usando el Gather esto es fundamental
+		//Acá le damos el tamaño de la lista que usaremos para guardar los datos en la raíz, usando el Gather, ésto es fundamental
 		sumas=(int*)malloc(sizeof(int)*1*size);
 		lista_aleatoria(100,&l);
 		
@@ -84,13 +84,13 @@ int main(int argc, char** argv) {
         }
 
 	//Hacemos el bcast para enviar los datos a todos los nodos, en este caso enviamos el promedio
-	//Tambien se hace enfasis en la direccion de la variable que se envia con el &, el tamaño de la variable, el tipo, los nodos y el comm
+	//También se hace énfasis en la dirección de la variable que se envia con el &, el tamaño de la variable, el tipo, los nodos y el comm
         MPI_Bcast(&promedio, 1, MPI_FLOAT, root, MPI_COMM_WORLD);
 
-	//En este caso, enviamos la lista, ya que la funcion que utilizamos para llenar la lista de numeros aleatorios la llamamos en el nodo maestro
+	//En este caso, enviamos la lista, ya que la función que utilizamos para llenar la lista de números aleatorios la llamamos en el nodo maestro
 	MPI_Bcast(&l,100,MPI_FLOAT,root,MPI_COMM_WORLD);
 	
-	//Hacemos la sumatoria, pero en forma paralela, definiendo los limites, en el caso de la lista 1, rank=0 recorrera de 0 a 24 tomando esos valores de la lista y restando el promedio total ya calculado. En el rank=1 parte de 25 y llegaria hasta 49 y asi sucesivamente
+	//Hacemos la sumatoria, pero en forma paralela, definiendo los límites, en el caso de la lista 1, rank=0 recorrera de 0 a 24 tomando esos valores de la lista y restando el promedio total ya calculado. En el rank=1 parte de 25 y llegaria hasta 49 y asi sucesivamente
 	for(int i = div_tamanho*rank  ; i < div_tamanho*(rank+1) ; i++){
 		sumatoria=((l[i]-promedio)*(l[i]-promedio))+sumatoria;
 	}  
@@ -98,12 +98,12 @@ int main(int argc, char** argv) {
 	//Mostramos los nodos y los argumentos de la sumatoria que estamos calculando, para enviarla al nodo maestro 
 	printf("%d %f\n",rank,sumatoria);
 
-	//Lejos lo que mas me complico, fue entender esta estructura, ya que no sabia manejar la lista donde recepcionaba los datos que enviaba al nodo maestro con Gather, es simple la estructura, ya que define los datos que envias con referencia a la direccion de memoria, tamaño de la variable y tipo de dato de la variable que se envia. Despues hay que hubicar la lista donde vas a recepcionar los datos que se envian, el tamaño de dato que envia cada nodo el tipo de dato que recibe, el root y el coom. Los ultimos 5 datos los maneja la raiz unicamente
+	//Lejos lo que mas me complico, fue entender esta estructura, ya que no sabia manejar la lista donde recepcionaba los datos que enviaba al nodo maestro con Gather, es simple la estructura, ya que define los datos que envias con referencia a la direccion de memoria, tamaño de la variable y tipo de dato de la variable que se envia. Despues hay que hubicar la lista donde vas a recepcionar los datos que se envian, el tamaño de dato que envia cada nodo el tipo de dato que recibe, el root y el coom. Los últimos 5 datos los maneja la raíz únicamente
 	MPI_Gather(&sumatoria, 1, MPI_FLOAT,sumas,1,MPI_FLOAT, root, MPI_COMM_WORLD);
 
 	if(rank==root){
 
-		//Aca revisaremos que los datos sean asignados al 100%. Un ejemplo de un dato no asignado es cuando tenemos 3 nodos, ya que dividiremos el numero de operaciones por el numero de procesadores 100/3 al el tipo de dato entero lo trunca, dejando la division de 33 operaciones por nodo, lo que en este caso particular dejaria 1 dato sin ser asignado a un nodo para que pueda operar con el.
+		//Acá revisaremos que los datos sean asignados al 100%. Un ejemplo de un dato no asignado es cuando tenemos 3 nodos, ya que dividiremos el número de operaciones por el número de procesadores 100/3 al el tipo de dato entero lo trunca, dejando la division de 33 operaciones por nodo, lo que en este caso particular dejaria 1 dato sin ser asignado a un nodo para que pueda operar con él.
 		if(reajuste!=0){
 			for(int i = 100-reajuste  ; i < 100 ; i++){
 				sumas_reajustes=((l[i]-promedio)*(l[i]-promedio))+sumas_reajustes;
