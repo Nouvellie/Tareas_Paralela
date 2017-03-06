c8e66ab91b94a5059466baa0650b2303d2d19b97
nicolasaguirre <nick.aguirre.16@gmail.com>
2017-03-06 10:21:00 -0300

    Conformidad aplicado a procesadores en paralelo

--- 4t_python_paralelo/solo_africa/conformidad_codigoMPI2.py
+++ 4t_python_paralelo/solo_africa/conformidad_codigoMPI2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math as mt
 from time import time
+from mpi4py import MPI
 
 #Calculamos distancia ingresando 2 vertices casteados en enteros y la lista lnodos
 def cal_dist(v1,v2,lnodos):
@@ -285,6 +286,14 @@ def part_a_pc(lelementos):
 		fp.write(str(1))
 	fp.close()
 
+def agregar_colaNodos(lista,posicion):
+	lista2=[None] * (len(lista)-posicion)
+	contador=0
+	for i in range(posicion,len(lista)):
+		lista2[contador]=lista[i]
+		contador=contador+1
+	return lista2
+
 #Verificamos la conformidad analizando los puntos medios de las 3 aristas, si estas se encuentran como vertice al menos uno, este triangulo no es conforme
 def conformidad(lelementos,lnodos, ultimo_indice):
 	i=1
@@ -311,7 +320,7 @@ def comp_pto_mdo(indice,pmdo_uno, pmdo_dos,pmdo_tres, lnodos, ultimo_indice):
 			return 1
 	return 0
 
-
+"""
 i=1
 #Recopilacion de datos
 contador=0
@@ -338,15 +347,117 @@ tiempo_ejecucion=tiempo_final-tiempo_inicial
 
 #cuatro_t(lelementos,return_indice_ele(lelementos,2))
 
-"""for i in range(1,int(lnodos[0][0])+1):
-	print lnodos[i][0]," ",lnodos[i][1], " ", lnodos[i][2]
-for i in range(1,int(lelementos[0][0])+1):
-	print lelementos[i][0]," ",lelementos[i][1], " ", lelementos[i][2], " ", lelementos[i][3], " ", lelementos[i][7]
-print ""
-print "Se van a refinar: ",cant_r, " triangulos."
-print ""
-print "Se refinaron: ", contador, " triangulos."	"""
 ele_a_pc(lelementos)
 node_a_pc(lnodos)
 part_a_pc(lelementos)
 print "Tiempo de ejecucion del refinamiento: ", tiempo_ejecucion
+"""
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+root=0
+#Cargamos los datos necesarios para aplicar los diferentes criterios. Esto es parte del preproceso
+#Recopilacion de datos
+contador=0
+
+lnodos=leer_node()
+uif = ultimo_indice_fijo(lnodos)
+#print uif
+lelementos=leer_ele()
+cal_ang(lnodos,lelementos)
+cant_r=crit_ref(lelementos,45)
+arista_larga(lelementos)
+asig_pto_mdo(lelementos)
+pto_opuesto(lelementos)
+
+lelementos.pop(0)
+lelementos=np.array(lelementos,dtype=object)
+
+posicion=int(lnodos[0][0])+1
+
+contador_ref = 0
+data=[]
+a_refinar=[]
+
+#Es importante este tamanho, ya que con esto verificaremos la conformidad de la interseccion de la malla
+tamanho_anterior=len(lelementos)
+tamanho_creado=0
+condicion_conformidad=0
+
+tiempo_inicial=time()
+#En el nodo maestro vamos a definir la lista a enviar por Scatter
+
+if rank == root:
+	
+	#Formas la lista de triangulos a enviar por scatter
+	a_refinar=np.array_split(lelementos,size)
+
+data = comm.scatter(a_refinar, root=root)
+lnodos=comm.bcast(lnodos,root=root)
+
+data2=[None] * (len(data)+1)
+data2[0]=[len(data),3]
+for i in range(1,len(data2)):
+	data2[i]=data[i-1]
+#data=np.insert(data,0,1)
+
+tiempo_inicial=time()
+
+conformidad(data2,lnodos, uif)
+
+lnodos_xrank=comm.gather(lnodos,root=root)
+lelementos_xrank=comm.gather(data2,root=root)
+
+if rank == root:
+
+	for t in range(0,len(lnodos_xrank)):
+		if t != 0:
+			lnodos_xrank[t]=agregar_colaNodos(lnodos_xrank[t],posicion)
+			for j in range(0,len(lnodos_xrank[t])):
+				lnodos.append(lnodos_xrank[t][j])
+		else:
+			lnodos=lnodos_xrank[t]
+			lnodos_xrank[t]=agregar_colaNodos(lnodos_xrank[t],posicion)
+	lnodos[0][0]=len(lnodos)-1
+	for t in range(1,len(lnodos)):
+		lnodos[t][0]=t
+
+	acumulado=len(lnodos_xrank[0])
+	for t in range(0,len(lelementos_xrank)):
+		if t!=0:
+			for j in range(1,len(lelementos_xrank[t])):
+				if int(lelementos_xrank[t][j][1]) > posicion-1:
+					lelementos_xrank[t][j][1]=int(lelementos_xrank[t][j][1])+acumulado
+				if int(lelementos_xrank[t][j][2]) > posicion-1:
+					lelementos_xrank[t][j][2]=int(lelementos_xrank[t][j][2])+acumulado
+				if int(lelementos_xrank[t][j][3]) > posicion-1:
+					lelementos_xrank[t][j][3]=int(lelementos_xrank[t][j][3])+acumulado
+			acumulado=acumulado+len(lnodos_xrank[t])
+
+	lelementos=lelementos_xrank[0]
+	for t in range(1,len(lelementos_xrank)):
+		for j in range(1,len(lelementos_xrank[t])):
+			lelementos.append(lelementos_xrank[t][j])
+
+	lelementos[0][0]=len(lelementos)-1
+	for t in range(1,len(lelementos)):
+		lelementos[t][0]=t
+
+	for le in lelementos:
+		print le
+	print "elementos totales refinados"
+	print ""
+
+	for ln in lnodos:
+		print ln
+	print "nodos generados"
+	print " "
+	ele_a_pc(lelementos)
+	node_a_pc(lnodos)
+	part_a_pc(lelementos)
+	tiempo_final=time()
+	tiempo_ejecucion=tiempo_final-tiempo_inicial
+	print "Tiempo de ejecucion del refinamiento: ", tiempo_ejecucion
+#print data2[-1]
